b3a32c9c1fa62416dc5a17fcd2bfb7ed783f3800
junyeoplee <junyeoplee91@gmail.com>
2018-01-08 15:18:52 +0900

    edit config for local

--- capsNet/config.py
+++ capsNet/config.py
@@ -1,8 +1,6 @@
 class Config:
     batch_size = 128
     learning_rate = 1e-3
-    logdir = 'gs://weighty-actor-177008-mlengine/capsNet_11_dr/logdir'
-    dataset = 'gs://weighty-actor-177008-mlengine/data/data/mnist/npy'
-    # logdir = 'logdir/logdir'
-    # dataset = 'data/mnist'
+    logdir = 'logdir/'
+    dataset = 'data/mnist'
     num_epochs = 500
